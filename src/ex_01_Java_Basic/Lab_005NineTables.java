48efd7ecc750d1155f5a640435e95186c2688717
ADMIN <sevenjoel@gmail.com>
2025-03-11 21:36:34 -0400

    11th March Sec9 Table Commit

---
+++ src/ex_01_Java_Basic/Lab_005NineTables.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab_005NineTables {
+    public static void main(String[] args) {
+    int num = 9;
+        System.out.printf("%d x %d = %d\n", num, 1, num * 1);
+        System.out.printf("%d x %d = %d\n", num, 2, num * 2);
+        System.out.printf("%d x %d = %d\n", num, 3, num * 3);
+        System.out.printf("%d x %d = %d\n", num, 4, num * 4);
+        System.out.printf("%d x %d = %d\n", num, 5, num * 5);
+        System.out.printf("%d x %d = %d\n", num, 6, num * 6);
+        System.out.printf("%d x %d = %d\n", num, 7, num * 7);
+        System.out.printf("%d x %d = %d\n", num, 8, num * 8);
+        System.out.printf("%d x %d = %d\n", num, 9, num * 9);
+        System.out.printf("%d x %d = %d\n", num, 10, num * 10);
+}
+}
